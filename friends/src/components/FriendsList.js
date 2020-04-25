af4e3191b59f14cabc274d953854a02a0f535d9e
Erik Scow <erikscow@gmail.com>
2020-04-24 20:41:42 -0700

    Data from server called via axiosWithAuth and displayed in component.

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
 
 import AddNewFriend from './AddNewFriend'
 
-const FriendsList = props => {
+const FriendsList = () => {
+
+    const [friends, setFriends] = useState([])
+
+    useEffect(() => {
+        axiosWithAuth()
+            .get('/friends')
+            .then(res => {
+                setFriends(res.data)
+            })
+    }, [])
+
     return(
         <div>
             <AddNewFriend />
+            {friends.map( friend => {
+                return (
+                    <div key={friend.id}>
+                        <h2>{friend.name}</h2>
+                        <h3>{friend.age}</h3>
+                        <h3>{friend.email}</h3>
+                    </div>
+                )
+            })}
             
         </div>
     )
