d90164038a108bcb2fbdc163f28f7baf14852f8a
Erik Scow <erikscow@gmail.com>
2020-04-24 18:56:37 -0700

    Added routes and components.

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,24 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+
+import PrivateRoute from './components/PrivateRoute'
+
+import Login from './components/Login'
+import FriendsList from './components/FriendsList'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Login to see friends.</h1>
+        <div className='nav'>
+          <Link to='/friends'>Friends</Link>
+        </div>
+        <Switch>
+          <Route path='/login' component={Login}/>
+          <PrivateRoute path='/friends' component={FriendsList} />
+        </Switch>
+        
+      </div>
+    </Router>
   );
 }
 
---
+++ friends/src/components/AddNewFriend.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddNewFriend = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default AddNewFriend
